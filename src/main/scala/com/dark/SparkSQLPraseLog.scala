d80dd1b71192655f9aafd2279ea0c97fb9037929
teng.xue <teng.xue@tcl.com>
2016-10-31 17:21:35 +0800

    增加spark sql 示例

--- src/main/scala/com/dark/SparkSQLPraseLog.scala
+++ src/main/scala/com/dark/SparkSQLPraseLog.scala
@@ -19,11 +19,12 @@ object SparkSQLPraseLog {
 
     val sqlContext=new  SQLContext(sc)
     val df=sqlContext.read.json(jsonData)
-    df.schema.add("us.ai","String",true)
-    df.show()
     df.registerTempTable("datalog")
-    val sql="select us.ai,us.am,us.appkey,us.c,us.cc,us.ch,us.dateTime,us.fr,us.h,us.im,us.ir,us.la,us.mac, from datalog"
-    sqlContext.sql(sql).show()
+    val sql="select us.ai,us.am,us.appkey,us.c,us.cc,us.ch,us.dateTime,us.fr,us.h,us.im,us.ir,us.la,us.mac,us.om,us.pn,us.sv,us.t,us.w,se.e,se.s from datalog"
+//    sqlContext.sql(sql).show()
+    val ai="ddQ6bLC0TYXPd57Y"
+    val seSql="select log.se.e ,log.se.s from datalog as log where log.us.ai='ddQ6bLC0TYXPd57Y'"
+    sqlContext.sql(seSql).foreach(println)
 
 
   }
