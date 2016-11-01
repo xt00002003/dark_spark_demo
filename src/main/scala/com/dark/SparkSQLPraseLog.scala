a6c2642fa1bb2ec64703ddaacf6a98254ca89c58
teng.xue <teng.xue@tcl.com>
2016-11-01 18:39:34 +0800

    增加spark sql 示例

--- src/main/scala/com/dark/SparkSQLPraseLog.scala
+++ src/main/scala/com/dark/SparkSQLPraseLog.scala
@@ -1,7 +1,8 @@
 package com.dark
 
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.{SQLContext, functions}
 import org.apache.spark.{SparkConf, SparkContext}
+
 
 /**
   * Created by darkxue on 25/10/16.
@@ -12,7 +13,7 @@ object SparkSQLPraseLog {
     val conf=new SparkConf().setMaster("local").setAppName("SparkSQLFirstProgrammer")
     val sc=new SparkContext(conf)
 
-    val jsonData=sc.textFile(dataPath+"CheckData.1474300800000").map(line=>{
+    val jsonData=sc.textFile(dataPath+"test.1474300800000").map(line=>{
        val array=line.split("[|]")
        array(2)
     })
@@ -24,7 +25,22 @@ object SparkSQLPraseLog {
 //    sqlContext.sql(sql).show()
     val ai="ddQ6bLC0TYXPd57Y"
     val seSql="select log.se.e ,log.se.s from datalog as log where log.us.ai='ddQ6bLC0TYXPd57Y'"
-    sqlContext.sql(seSql).foreach(println)
+//    sqlContext.sql(seSql).foreach(println)
+//    df.select(df("us.ai"),functions.explode(df("se"))).filter("us.ai='ddQ6bLC0TYXPd57Y'").show()
+//    df.where(df("se.e").isNull).withColumn("se.e",functions.lit(null))
+//      .unionAll(df.withColumn("se.e",functions.explode(df("se.e"))))
+//        .select("us.ai","se.e")
+//      .show()
+//    df.filter("us.ai='ddQ6bLC0TYXPd57Y'").select(df("us.ai"),functions.explode(df("se.e"))).show()
+    val notNULLDF=df.select(df("us.ai"),functions.explode(df("se.e")))
+    println("not null count is :"+notNULLDF.count())
+    notNULLDF.show()
+    val nullDF=df.where(df("se.e").isNull)
+      .withColumn("se.e",functions.lit(null))
+      .select("_corrupt_record")
+    println("null count is :"+nullDF.count())
+    nullDF.show()
+    nullDF.printSchema()
 
 
   }
